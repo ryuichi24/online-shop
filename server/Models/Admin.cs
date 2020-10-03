e4bd4929b2f35045bb5a1c02d336598970ef03f4
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-03 16:38:20 +0300

    added an Admin model

---
+++ server/Models/Admin.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+    }
+}

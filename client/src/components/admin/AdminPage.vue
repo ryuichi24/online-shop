e68010d4f83771bc4a7fcf9979d347956ce83e02
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-06 14:00:57 +0300

    added dashboard comp

--- client/src/components/admin/AdminPage.vue
+++ client/src/components/admin/AdminPage.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>Admin</div>
-  <LoginForm />
+  <LoginForm v-if="!isAdmin" />
+  <TheDashboard v-if="isAdmin" />
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+// vuex
+import { useStore } from 'vuex';
 // components
 import LoginForm from './login/LoginForm.vue';
+import TheDashboard from './dashboard/TheDashboard.vue';
 
 export default defineComponent({
   components: {
     LoginForm,
+    TheDashboard,
+  },
+  setup() {
+    const { getters } = useStore();
+
+    const isAdmin = computed<boolean>(() => getters.isAdmin);
+
+    return {
+      isAdmin,
+    };
   },
 });
 </script>

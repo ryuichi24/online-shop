82222cfd7066559dedbc9e14f8e0929ee65ce755
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-07 16:43:24 +0300

    connected reactive opject to each input in product form

--- client/src/components/admin/dashboard/product-form/ProductForm.vue
+++ client/src/components/admin/dashboard/product-form/ProductForm.vue
@@ -2,30 +2,31 @@
   <teleport to="#teleportCreateProduct">
     <modal-wrapper>
       <span class="close-btn" @click="toggleForm">&times;</span>
-      <form @submit.prevent="" class="product-form">
+      <form @submit.prevent="addNewProduct" class="product-form">
         <div class="product-form__input-container">
           <label for="name">Name</label>
-          <input name="name" type="text" />
+          <input v-model="name" name="name" type="text" />
         </div>
         <div class="product-form__input-container">
           <label for="price">Price</label>
-          <input name="price" type="number" step="0.01" />
+          <input v-model="price" name="price" type="number" step="0.01" />
         </div>
         <div class="product-form__input-container">
           <label for="description">Description</label>
-          <textarea name="description" cols="30" rows="10"></textarea>
+          <textarea v-model="description" name="description" cols="30" rows="10"></textarea>
         </div>
         <div class="product-form__input-container">
           <label for="inventory">Inventory</label>
-          <input name="inventory" type="number" />
+          <input v-model="inventory" name="inventory" type="number" />
         </div>
         <div class="product-form__category-input-container">
           <label for="category-list">Category</label>
-          <select name="category-list">
+          <select v-model="categoryId" name="category-list">
+            <option value="default">-- Choose a category --</option>
             <option
               v-for="(category, index) in categories"
               :key="index"
-              value="category.categoryId"
+              :value="category.categoryId"
               >{{ category.name }}</option
             >
           </select>
@@ -38,7 +39,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, onMounted } from 'vue';
+import { defineComponent, computed, onMounted, reactive, toRefs } from 'vue';
 // vuex
 import { useStore } from 'vuex';
 import { GET_CATEGORIES } from '../../../../store/types/action.type';
@@ -57,6 +58,18 @@ export default defineComponent({
   setup() {
     const { getters, dispatch } = useStore();
 
+    const productInputs = reactive({
+      name: '',
+      price: 0,
+      description: '',
+      inventory: 0,
+      categoryId: 'default',
+    });
+
+    const addNewProduct = () => {
+      console.log(productInputs);
+    };
+
     const categories = computed(() => getters.categories);
 
     onMounted(() => {
@@ -65,6 +78,8 @@ export default defineComponent({
 
     return {
       categories,
+      ...toRefs(productInputs),
+      addNewProduct,
     };
   },
 });

14340718a1c01a3d488f1b8f1ec14accc0558cbd
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-07 18:38:01 +0300

    added product controller for client and made addProduct function

---
+++ client/src/controllers/product.controller.ts
@@ -0,0 +1,23 @@
+import { AxiosError } from 'axios';
+// services
+import ApiService from '@/services/api.service';
+// types
+import { Product, ServerError } from '@/types';
+
+const addProduct = async (product: Product) => {
+  try {
+      const { data } = await ApiService.API.post<Product>('/product', product);
+
+      return data;
+  } catch (err) {
+    if (err && err.response) {
+        const axiosError = err as AxiosError<ServerError>;
+        if (axiosError.response) return axiosError.response.data;
+      }
+      throw err;
+  }
+};
+
+export default {
+  addProduct,
+};
--- client/src/types/product/index.d.ts
+++ client/src/types/product/index.d.ts
@@ -1,5 +1,5 @@
 export interface Product {
-    productId: number;
+    productId?: number;
     name: string;
     price: number;
     description: string;

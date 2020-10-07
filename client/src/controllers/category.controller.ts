10c8b1d03a3fd68dd5b671112ba5a32fef4af3a1
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-07 16:12:38 +0300

    added getCategories controller function

--- client/src/controllers/category.controller.ts
+++ client/src/controllers/category.controller.ts
@@ -4,6 +4,20 @@ import ApiService from '@/services/api.service';
 // types
 import { Category, ServerError } from '@/types';
 
+const getCategories = async () => {
+  try {
+    const { data } = await ApiService.API.get('/category');
+
+    return data;
+  } catch (err) {
+    if (err && err.response) {
+      const axiosError = err as AxiosError<ServerError>;
+      if (axiosError.response) return axiosError.response.data;
+    }
+    throw err;
+  }
+};
+
 const addCategory = async (name: string) => {
   try {
     const { data } = await ApiService.API.post<{ category: Category }>('/category', { name });
@@ -18,5 +32,6 @@ const addCategory = async (name: string) => {
 };
 
 export default {
+  getCategories,
   addCategory,
 };

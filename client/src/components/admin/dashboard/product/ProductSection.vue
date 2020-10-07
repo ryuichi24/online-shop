8c20075e0d787440b9254527c668433803d94309
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-07 11:55:12 +0300

    made products admin page

--- client/src/components/admin/dashboard/product/ProductSection.vue
+++ client/src/components/admin/dashboard/product/ProductSection.vue
@@ -1,11 +1,91 @@
 <template>
-  <div>
-      products sectiond
+  <div class="product-section">
+    <div class="product-section__heading-container">
+      <h1>PRODUCTS</h1>
+    </div>
+    <div class="product-section__product-list-container">
+      <div class="product-section__product" v-for="(product, index) in products" :key="index">
+        {{ product.name }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { defineComponent, ref } from 'vue';
 
-};
+export default defineComponent({
+  setup() {
+    const products = ref([
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+      {
+        name: 'product 1',
+        price: '10',
+      },
+    ]);
+
+    return {
+      products,
+    };
+  },
+});
 </script>

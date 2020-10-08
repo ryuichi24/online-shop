42059a7ac48d85c4cccc6034cfb4cf8ea0605375
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-08 17:40:31 +0300

    removed useModalToggle

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,6 +1,5 @@
 import useNavItems from './useNavItems';
 import useUtils from './useUtils';
 import useSidebarItems from './useSidebarItem';
-import useModalToggle from './useModalToggle';
 
-export { useNavItems, useUtils, useSidebarItems, useModalToggle };
+export { useNavItems, useUtils, useSidebarItems };

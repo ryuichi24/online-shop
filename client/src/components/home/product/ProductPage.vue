f8ce31bab6d9c7cfbec08caddd9c999293b649f0
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-11 12:27:39 +0300

    designed product page a little

--- client/src/components/home/product/ProductPage.vue
+++ client/src/components/home/product/ProductPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="container" style="margin-bottom: 10rem;">
-    {{ product }}
+  <div class="container">
+    <div v-if="product" class="product-page">
+      <div class="product-page__section">
+        <div>
+          <img src="https://source.unsplash.com/random" alt="product image">
+        </div>
+      </div>
+      <div class="product-page__section">
+        <h2>{{ product.name }}</h2>
+        <hr>
+        <p>€{{ product.price }}</p>
+        <p>{{ product.description }}</p>
+        <p v-if="product.inventory > 0">In stock</p>
+        <p v-else>Out of stock</p>
+      </div>
+    </div>
   </div>
 </template>
 

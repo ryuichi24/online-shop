fa3bfcdd48c9056e7e576330afa0dff6abd443b9
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-11 20:18:51 +0300

    added CartItem comp

---
+++ client/src/components/cart-items/CartItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cart-item">
+    <h2>{{ cartItem.product.name }}</h2>
+    <p>{{ cartItem.cartItemCount }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    cartItem: Object,
+  },
+});
+</script>
--- client/src/components/cart-items/CartItemsPage.vue
+++ client/src/components/cart-items/CartItemsPage.vue
@@ -1,9 +1,6 @@
 <template>
   <div v-if="cartItems" class="container" style="margin-bottom: 10rem;">
-    <div v-for="(item, index) in cartItems" :key="index">
-      <h2>{{ item.product.name }}</h2>
-      <p>{{ item.cartItemCount }}</p>
-    </div>
+    <CartItem v-for="(item, index) in cartItems" :key="index" :cartItem="item" />
   </div>
 </template>
 
@@ -12,8 +9,13 @@ import { defineComponent, computed, onMounted } from 'vue';
 // vuex
 import { useStore } from 'vuex';
 import { GET_ALL_CART_ITEMS_BY_USER_ID } from '../../store/types/action.type';
+// components
+import CartItem from './CartItem.vue';
 
 export default defineComponent({
+  components: {
+    CartItem,
+  },
   setup() {
     const { dispatch, getters } = useStore();
 

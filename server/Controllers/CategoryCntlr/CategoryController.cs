a606006d47d444ec21e2db97c5243bd2e603e73a
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-11 16:22:00 +0300

    updated category controller

--- server/Controllers/CategoryCntlr/CategoryController.cs
+++ server/Controllers/CategoryCntlr/CategoryController.cs
@@ -3,14 +3,20 @@ using server.Helpers.ParameterClass;
 using server.Models;
 using server.DataAccess.Repositories.CategoryRepo;
 using server.Helpers.CustomResponse;
+using System.Collections.Generic;
 
 namespace server.Controllers.CategoryCntlr
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoryController : RootController<Category, ICategoryRepository>, ICategoryController
+    public class CategoryController : ControllerBase, ICategoryController
     {
-        public CategoryController(ICategoryRepository repository) : base(repository) { }
+        private readonly ICategoryRepository _repository;
+
+        public CategoryController(ICategoryRepository repository)
+        {
+            this._repository = repository;
+        }
 
         [HttpPost]
         public ActionResult<Category> AddNewCategory([FromBody] CategoryCreateParameter categoryCreateParameter)
@@ -24,6 +30,34 @@ namespace server.Controllers.CategoryCntlr
             this._repository.SaveChanges();
 
             return this.CreatedAtRoute(new { Id = newCategory.CategoryId }, newCategory);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCategory(int id)
+        {
+            Category category = this._repository.GetById(id);
+            if (category == null) return NotFound();
+
+            this._repository.Remove(category);
+            this._repository.SaveChanges();
+
+            return this.NoContent();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetAllCategories()
+        {
+            return this.Ok(this._repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Category> GetCategoryById(int id)
+        {
+            Category category = this._repository.GetById(id);
+
+            if (category == null) return NotFound();
+
+            return this.Ok(category);
         }
 
         [HttpPut("{id}")]
--- server/Controllers/CategoryCntlr/ICategoryController.cs
+++ server/Controllers/CategoryCntlr/ICategoryController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using server.Helpers.CustomResponse;
 using server.Helpers.ParameterClass;
@@ -7,6 +8,12 @@ namespace server.Controllers.CategoryCntlr
 {
     public interface ICategoryController
     {
+        public ActionResult<IEnumerable<Category>> GetAllCategories();
+
+        public ActionResult<Category> GetCategoryById(int id);
+
+        public ActionResult DeleteCategory(int id);
+
         public ActionResult<Category> AddNewCategory([FromBody] CategoryCreateParameter categoryCreateParameter);
 
         public ActionResult UpdateCategory(int id, [FromBody] CategoryUpdateParameter categoryUpdateParameter);

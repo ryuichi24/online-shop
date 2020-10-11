8fe5411b4b2eeb407eb0ccaa652aca17b2b72428
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-11 11:24:43 +0300

    added AddNewCartItem and UpdateCartItem api actions

--- server/Controllers/CartItemCntlr/CartItemController.cs
+++ server/Controllers/CartItemCntlr/CartItemController.cs
@@ -11,14 +11,34 @@ namespace server.Controllers.CartItemCntlr
     {
         public CartItemController(ICartItemRepository repository) : base(repository) { }
 
+        [HttpPost]
         public ActionResult<CartItem> AddNewCartItem([FromBody] CartItemCreateParameter cartItemCreateParameter)
         {
-            throw new System.NotImplementedException();
+            CartItem newCartItem = new CartItem()
+            {
+                ProductId = cartItemCreateParameter.ProductId,
+                UserId = cartItemCreateParameter.UserId,
+                CartItemCount = cartItemCreateParameter.CartItemCount
+            };
+
+            this._repository.Add(newCartItem);
+            this._repository.SaveChanges();
+
+            return this.CreatedAtRoute(new { Id = newCartItem.CardItemId }, newCartItem);
         }
 
+        [HttpPut("{id}")]
         public ActionResult UpdateCartItem(int id, [FromBody] CartItemUpdateParameter cartItemUpdateParameter)
         {
-            throw new System.NotImplementedException();
+            CartItem existingCartItem = this._repository.GetById(id);
+            if (existingCartItem == null) return this.NotFound();
+
+            if (!cartItemUpdateParameter.CartItemCount.Equals(null)) existingCartItem.CartItemCount = cartItemUpdateParameter.CartItemCount;
+
+            this._repository.Update(existingCartItem);
+            this._repository.SaveChanges();
+
+            return this.NoContent();
         }
     }
 }

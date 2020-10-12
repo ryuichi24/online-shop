f65179bb1e65124cb56e74784fffb9a8c7cb38d4
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-12 12:32:22 +0300

    added Address interface

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -3,6 +3,7 @@ import { Admin } from './admin';
 import { Category } from './category';
 import { Product } from './product';
 import { CartItem } from './cartItem';
+import { Address } from './address';
 import { ServerError } from './error';
 
-export { User, Admin, Category, Product, CartItem, ServerError };
+export { User, Admin, Category, Product, CartItem, Address, ServerError };

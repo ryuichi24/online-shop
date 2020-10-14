1f292932f4f1e3ff113505fa1b83ca80de611cb9
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-14 13:17:30 +0300

    added accountt page layout

--- client/src/components/account/AccountPage.vue
+++ client/src/components/account/AccountPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="container" style="margin-bottom: 10rem;">Account</div>
+  <div class="container" style="margin-bottom: 10rem;">
+    <div class="account-page">
+      <div class="account-page__card account-page__info">Account Info</div>
+      <div class="account-page__card account-page__orders">Orders</div>
+      <div class="account-page__card account-page__cart-items">CartItems</div>
+      <div class="account-page__logout-contact-us">
+        <div class="account-page__card">Logout</div>
+        <div class="account-page__card">Contact us</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>

211015a9b2e0b7f879463f95368f04c9870e02c0
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-14 14:16:21 +0300

    added logoutt btn

--- client/src/components/account/AccountPage.vue
+++ client/src/components/account/AccountPage.vue
@@ -5,13 +5,36 @@
       <div class="account-page__card account-page__orders">Orders</div>
       <div class="account-page__card account-page__cart-items">CartItems</div>
       <div class="account-page__logout-contact-us">
-        <div class="account-page__card">Logout</div>
-        <div class="account-page__card">Contact us</div>
+        <div class="account-page__card">
+          <span>Contact us</span>
+        </div>
+        <div class="account-page__logout-btn-container">
+          <span class="account-page__logout-btn" @click="logout">Logout</span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+// vuex
+import { useStore } from 'vuex';
+import { LOGOUT_USER } from '../../store/types/action.type';
+
+export default defineComponent({
+  setup() {
+    const { push } = useRouter();
+    const { dispatch } = useStore();
+
+    const logout = () => {
+      dispatch(LOGOUT_USER).then(() => push({ name: 'LoginForm' }));
+    };
+
+    return {
+      logout,
+    };
+  },
+});
 </script>

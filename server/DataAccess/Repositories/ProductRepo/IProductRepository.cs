a1e6fac648982e148530ec80a655870eaf8432c5
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-10-17 00:22:04 +0300

    added GetAllProductByCategoryId function

--- server/DataAccess/Repositories/ProductRepo/IProductRepository.cs
+++ server/DataAccess/Repositories/ProductRepo/IProductRepository.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using server.Models;
 
 namespace server.DataAccess.Repositories.ProductRepo
 {
     public interface IProductRepository : IRepository<Product>
     {
-         // add some features specially for Product
+        // add some features specially for Product
+        IEnumerable<Product> GetAllProductsByCategoryId(int categoryId);
     }
 }
--- server/DataAccess/Repositories/ProductRepo/ProductRepository.cs
+++ server/DataAccess/Repositories/ProductRepo/ProductRepository.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using server.DataAccess;
 using server.Models;
 
@@ -6,5 +8,11 @@ namespace server.DataAccess.Repositories.ProductRepo
     public class ProductRepository : Repository<Product>, IProductRepository
     {
         public ProductRepository(OnlineShopDbContext dbContext) : base(dbContext) { }
+
+        public IEnumerable<Product> GetAllProductsByCategoryId(int categoryId)
+        {
+            return this._DbContext.Set<Product>().Where(p => p.CategoryId == categoryId)
+            .ToList();
+        }
     }
 }

275e5deda20f6961057d64e24148b921c534f729
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-11-01 12:14:50 +0200

    added ServiceCollectionExtension to clean up startup

---
+++ server/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using server.DataAccess;
+using server.DataAccess.Repositories.AddressRepo;
+using server.DataAccess.Repositories.AdminRepo;
+using server.DataAccess.Repositories.CartItemRepo;
+using server.DataAccess.Repositories.CategoryRepo;
+using server.DataAccess.Repositories.OrderItemRepo;
+using server.DataAccess.Repositories.OrderRepo;
+using server.DataAccess.Repositories.ProductRepo;
+using server.DataAccess.Repositories.UserRepo;
+using server.Helpers;
+using server.Services.Auth;
+
+namespace server.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<ICartItemRepository, CartItemRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection SetupController(this IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson
+            (
+                options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+            return services;
+        }
+
+        public static IServiceCollection SetupCors(this IServiceCollection services)
+        {
+            services.AddCors(options => options.AddDefaultPolicy
+            (
+                builder => builder.WithOrigins(Environment.GetEnvironmentVariable("CLIENT_ORIGIN") ?? "http://localhost:8080")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+            ));
+
+            return services;
+        }
+
+        public static IServiceCollection SetupAuthentication(this IServiceCollection services, IConfigurationSection appSettingsSection)
+        {
+            // JWT authentication
+            string jwtSecret = appSettingsSection.Get<AppSettings>().JwtSecret;
+            services.AddSingleton<IAuthManager, AuthManager>();
+            services.AddAuthentication
+            (
+                x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }
+            )
+            .AddJwtBearer
+            (
+                x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSecret)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                }
+            );
+
+            return services;
+        }
+
+        public static IServiceCollection SetupDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OnlineShopDbContext>
+                    (
+                        options => options.UseSqlServer
+                        (
+                            configuration.GetConnectionString("DatabaseConnection")
+                        )
+                    );
+
+            return services;
+        }
+
+        public static IServiceCollection SetupSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "Online Shop API" });
+
+                opt.AddSecurityDefinition
+                (
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = @"Enter 'Bearer <token>'",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer"
+                    }
+                );
+
+                opt.AddSecurityRequirement
+                (
+                    new OpenApiSecurityRequirement()
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+
+                            },
+                            new List<string>()
+                        }
+
+                    }
+                );
+            });
+
+            return services;
+        }
+    }
+}
--- server/Startup.cs
+++ server/Startup.cs
@@ -1,35 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-// swagger
-using Swashbuckle.AspNetCore;
-using Microsoft.OpenApi.Models;
-// database
-using server.DataAccess;
-using Microsoft.EntityFrameworkCore;
 using server.Helpers;
-using server.Services.Auth;
-using server.DataAccess.Repositories.AdminRepo;
-using server.DataAccess.Repositories.AddressRepo;
-using server.DataAccess.Repositories.CartItemRepo;
-using server.DataAccess.Repositories.CategoryRepo;
-using server.DataAccess.Repositories.OrderRepo;
-using server.DataAccess.Repositories.ProductRepo;
-using server.DataAccess.Repositories.UserRepo;
-// jwt authentication
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using server.DataAccess.Repositories.OrderItemRepo;
+using server.Extensions;
 
 namespace server
 {
@@ -45,110 +21,17 @@ namespace server
         public void ConfigureServices(IServiceCollection services)
         {
             // AppSettings
-            IConfigurationSection appSettingsSection = Configuration.GetSection("AppSettings");
+            IConfigurationSection appSettingsSection = this.Configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
 
-            services.AddControllers().AddNewtonsoftJson
-            (
-                options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            );
-
-            // dependency injections
-            services.AddScoped<IAddressRepository, AddressRepository>();
-            services.AddScoped<IAdminRepository, AdminRepository>();
-            services.AddScoped<ICartItemRepository, CartItemRepository>();
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IOrderRepository, OrderRepository>();
-            services.AddScoped<IOrderItemRepository, OrderItemRepository>();
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped<IUserRepository, UserRepository>();
-
-            // CORS
-            services.AddCors(options => options.AddDefaultPolicy
-            (
-                builder => builder.WithOrigins(Environment.GetEnvironmentVariable("CLIENT_ORIGIN") ?? "http://localhost:8080")
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials()
-            ));
-
-            // JWT authentication
-            string jwtSecret = appSettingsSection.Get<AppSettings>().JwtSecret;
-            services.AddSingleton<IAuthManager, AuthManager>();
-            services.AddAuthentication
-            (
-                x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                }
-            )
-            .AddJwtBearer
-            (
-                x =>
-                {
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSecret)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                }
-            );
-
-            // database
-            services.AddDbContext<OnlineShopDbContext>
-                    (
-                        options => options.UseSqlServer
-                        (
-                            this.Configuration.GetConnectionString("DatabaseConnection")
-                        )
-                    );
-
-            // swagger
-            services.AddSwaggerGen(opt =>
-            {
-                opt.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "Online Shop API" });
-
-                opt.AddSecurityDefinition
-                (
-                    "Bearer",
-                    new OpenApiSecurityScheme
-                    {
-                        Description = @"Enter 'Bearer <token>'",
-                        Name = "Authorization",
-                        In = ParameterLocation.Header,
-                        Type = SecuritySchemeType.ApiKey,
-                        Scheme = "Bearer"
-                    }
-                );
-
-                opt.AddSecurityRequirement
-                (
-                    new OpenApiSecurityRequirement()
-                    {
-                        {
-                            new OpenApiSecurityScheme
-                            {
-                                Reference = new OpenApiReference
-                                {
-                                    Type = ReferenceType.SecurityScheme,
-                                    Id = "Bearer"
-                                },
-                                Scheme = "oauth2",
-                                Name = "Bearer",
-                                In = ParameterLocation.Header,
-
-                            },
-                            new List<string>()
-                        }
-
-                    }
-                );
-            });
+            // setup
+            services
+                .SetupController()
+                .RegisterServices()
+                .SetupCors()
+                .SetupDatabase(this.Configuration)
+                .SetupAuthentication(appSettingsSection)
+                .SetupSwagger();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
